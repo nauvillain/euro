0be7dc0f368ae0c092a044f3c15b388ae9966a57
Nicolas Auvillain <nauvillain@hotmail.com>
2016-05-03 23:43:42 -0400

    added a slash ..

--- lib/jpgraph/src/jpg-config.inc.php
+++ lib/jpgraph/src/jpg-config.inc.php
@@ -36,7 +36,7 @@
 //
 //------------------------------------------------------------------------
 // define('CACHE_DIR','/tmp/jpgraph_cache/');
- define('TTF_DIR','/usr/share/fonts/truetype');
+ define('TTF_DIR','/usr/share/fonts/truetype/');
 // define('MBTTF_DIR','/usr/share/fonts/truetype/');
 
 //-------------------------------------------------------------------------
