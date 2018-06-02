994bdd707536e01552c393768abb609b154c13c3
nauvillain <nauvillain@hotmail.com>
2018-06-01 22:40:26 -0400

    corrected a security flaw in auth_conf.php - no using of id in cookie

--- auth_foot.php
+++ auth_foot.php
@@ -7,11 +7,8 @@ if (!isset($_SESSION['count_sessions'])) {
    $_SESSION['count_sessions']++;
 }
 
-if((!isset($_COOKIE['login_id']))&&(!$_SESSION['login_id'])){
+if(!$_SESSION['login_id']){
 	header("Location:login.php?url=".rawurlencode($REQUEST_URI));
-} else {
-  if (isset($_COOKIE['login_id'])) $login_id=$_COOKIE['login_id'];
-  else $login_id=$_SESSION['login_id'];
-}
+} else $login_id=$_SESSION['login_id'];
 
 ?>
